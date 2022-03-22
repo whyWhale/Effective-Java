a14e9496f36827d3fef2bddc45dced8617e3fc4f
yeon <soccer_by@naver.com>
2022-03-22 23:49:22 +0900

    feat: 셈플 코드 작성

---
+++ app/src/main/java/com/programmers/java/item1/Dummy.java
@@ -0,0 +1,5 @@
+package com.programmers.java.item1;
+
+public class Dummy {
+
+}
---
+++ app/src/main/java/com/programmers/java/item1/code/yeon/Dummy.java
@@ -0,0 +1,9 @@
+package com.programmers.java.item1.code.yeon;
+
+public class Dummy {
+
+  public static void main(String[] args) {
+    System.out.println("dummy data");
+  }
+
+}
